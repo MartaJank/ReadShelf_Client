357805fac3379277d33f91dc809550939a59a2b5
Marta Jankowska <martajan218@gmail.com>
2020-12-01 22:55:06 +0100

    added state to profile page but still not rendering updated

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -3,14 +3,23 @@ import { withAuth } from "../lib/AuthProvider";
 import { Link } from "react-router-dom";
 
 class Private extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: this.props.user.username,
+      email: this.props.user.email,
+      imageUrl: this.props.user.imageUrl,
+    };
+  }
+
   render() {
     return (
       <div>
         <div className="profile-pic-div">
-          <img className="profile-pic" src={this.props.user.imageUrl} />
+          <img className="profile-pic" src={this.state.imageUrl} />
         </div>
         <div className="user-info">
-          <h2 className="username">{this.props.user.username}</h2>
+          <h2 className="username">{this.state.username}</h2>
           <Link to={`/profile/${this.props.user._id}/edit`}>
             <button className="profile-edit-btn">Edit</button>
           </Link>
