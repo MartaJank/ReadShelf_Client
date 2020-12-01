43b3065133a530719a36c96067c9f64cda33aecd
Marta Jankowska <martajan218@gmail.com>
2020-12-01 22:12:24 +0100

    Profile changed

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 import { withAuth } from "../lib/AuthProvider";
+import { Link } from "react-router-dom";
 
 class Private extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome {this.props.user.username}</h1>
+        <div className="profile-pic-div">
+          <img className="profile-pic" src={this.props.user.imageUrl} />
+        </div>
+        <div className="user-info">
+          <h2 className="username">{this.props.user.username}</h2>
+          <Link to={`/profile/${this.props.user._id}/edit`}>
+            <button className="profile-edit-btn">Edit</button>
+          </Link>
+        </div>
+        <div className="buttons-div">
+          <Link to={"/bookshelf/paper"}>
+            <button className="profile-btn">My Bookshelf</button>
+          </Link>
+          <Link to={"/tracking/pending"}>
+            <button className="profile-btn">Reads Tracking</button>
+          </Link>
+          <Link to={`/book-clubs/${this.props.user._id}/created`}>
+            <button className="profile-btn">My Book Clubs</button>
+          </Link>
+          <Link to={`/book-clubs/${this.props.user._id}/joined`}>
+            <button className="profile-btn">Joined Book Clubs</button>
+          </Link>
+        </div>
       </div>
     );
   }
